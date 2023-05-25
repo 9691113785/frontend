4a7857f5c163e3a2b8464605e924730f9d573cb2
9691113785 <alokkushwaha96300@gmail.com>
2023-05-26 00:36:58 +0530

    third

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import '../Styles/Navbar.css';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { store } from '../App';
+// import axios from 'axios';
+// import { store } from '../App';
 
 const Navbar = () => {
     const navigate = useNavigate();
-    const [getdataa, sgetdata] = useState([]);
+    // const [getdataa, sgetdata] = useState([]);
 
-const context = useContext(store)
+// const context = useContext(store)
     const userdetails = () =>{
-      axios.get('http://localhost:8000/showdata').then((res)=>{
-        console.log(res.data)
-        sgetdata(res.data)
-      })
-      .catch((err)=>console.log(err))
+      // axios.get('http://localhost:8000/showdata').then((res)=>{
+      //   console.log(res.data)
+      //   sgetdata(res.data)
+      // })
+      // .catch((err)=>console.log(err))
 
-      context.setDetails(getdataa)
+      // context.setDetails(getdataa)
         navigate('/studentdetails')
     }
     const adduser = () =>{
